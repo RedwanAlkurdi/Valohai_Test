31cb42fe2fb484da9efe3250f73259f131651256
redwanallkurdi <redwanallkurdi@gmail.com>
2021-04-22 15:23:43 +0200

    try 1000

--- predict.py
+++ predict.py
@@ -31,18 +31,24 @@ async def predict(image: UploadFile = File(...)):
 
     # Resize image and convert to grayscale
     img = img.resize((150,150)).convert('RGB')
+    print("one")
     img_array = numpy.array(img)
+    print("two")
     img_array = img_array * 1./255
+    print("three")
     image_data = numpy.expand_dims(img_array, axis=0)
+    print("four")
 
     global loaded_model
     # Check if model is already loaded
     if not loaded_model:
         loaded_model = tf.keras.models.load_model(model_path)
-
+    print("five")
     # Predict with the model
     prediction = loaded_model.predict(image_data)
+    print("six")
     p = prediction[0][0]
+    print("seven")
     if p > 0.5:
         return "prediction: human "
     if p < 0.5:
